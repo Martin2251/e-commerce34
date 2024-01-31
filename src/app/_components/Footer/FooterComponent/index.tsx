03d5bfd2602769a574aa33d75fb0bc8ec1a5dcbc
Martin Lowe <martinlowe673@gmail.com>
2024-01-31 19:30:06 +0000

    before modifying

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,18 +1,41 @@
 "use client"
 import React from 'react'
+import Image from 'next/image'
 import classes from "./index.module.scss"
-import { noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
-const FooterComponent = () => {
+import Link from 'next/link'
+import { Footer } from '../../../../payload/payload-types'
+
+const FooterComponent = ({footer}:{footer:Footer}) => {
   const pathname = usePathname()
   return (
 <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide :""}>
 <Gutter>
   <ul className={classes.inclusions}>
+    {inclusions.map((inclusion,index) => (
+      <li key={inclusion.title}>
+
+        <Image src={inclusion.icon} alt={inclusion.title} width={36} height={36} className={classes.icon}/>
+        <h5 className={classes.title}>{inclusion.title}</h5>
+        <p>{inclusion.description}</p>
+      </li>
+    ))}
 
   </ul>
 </Gutter>
+<div className={classes.footer}>
+<Gutter>
+  <div className={classes.wrap}>
+    <Link href="/">
+      <Image src="/logo-white.svg" alt='logo ' width={170} height={50}  />
+
+    </Link>
+    <p>{footer.copyright}</p>
+  </div>
+</Gutter>
+</div>
 </footer>
   )
 }
